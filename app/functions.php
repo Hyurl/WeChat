2e57f9c13f212056c8600cfb0a8fc7fbd9622d6d
hyurl <ayon@hyurl.com>
2017-06-08 20:02:43 +0800

    Update via git

--- app/functions.php
+++ app/functions.php
@@ -158,8 +158,8 @@ add_action('WeixinDev.message', function($input){
 	if($input['send']['MsgType'] == 'text' && $sendContent) return;
 	$history = WeixinDev::getHistory($input['recv']['FromUserName']); //获取对话历史
 	$history = isset($history[1]) ? $history[1] : '';
-	$anotherOne = strpos($recvContent, '再来') !== false && strpos($history, '笑话') !== false; //将“再来”识别为“笑话”
-	if(strpos($recvContent, '笑话') !== false || $anotherOne){
+	$anotherOne = strpos($recvContent, '再来') !== false && str_contents($history, '笑话') && str_contents($history, array('讲', '说', '来个')); //将“再来”识别为“笑话”
+	if(str_contents($recvContent, '笑话') && str_contents($recvContent, array('讲', '说', '来个')) || $anotherOne){
 		if($anotherOne) WeixinDev::setHistory($input['recv']['FromUserName'], $history); //设置历史纪录
 		$result = curl('http://xhkong.com/tag/'); //获取标签
 		if(preg_match_all('/<span(.*)<a href="\/tag\/(.*)" target="_blank">(.*)<\/a><\/span>/Ui', $result, $matches)){
@@ -194,84 +194,38 @@ add_action('WeixinDev.message', function($input){
 	}
 });
 
-/** 美文推荐 */
-add_action('WeixinDev.message', function($input){
-	$recvContent = &$input['recv']['Content'];
-	$sendContent = &$input['send']['Content'];
-	if(($input['send']['MsgType'] == 'text' && $sendContent) || !empty($input['send']['Articles'])) return;
-	$history = WeixinDev::getHistory($input['recv']['FromUserName']);
-	$history = isset($history[1]) ? $history[1] : '';
-	$anotherOne = (strpos($history, '美文') !== false || strpos($history, '文章') !== false) && (strpos($recvContent, '再来') !== false || strpos($recvContent, '再来一篇') !== false);
-	if(strpos($recvContent, '美文') !== false || strpos($recvContent, '文章') !== false || $anotherOne){
-		if($anotherOne) WeixinDev::setHistory($input['recv']['FromUserName'], $history);
-		$baseUrl = 'https://www.lookmw.cn';
-		$result = curl(array(
-			'url'=>$baseUrl,
-			'charset'=>'UTF-8',
-			));
-		if(preg_match_all('/<ul class="picAtc pl">([\s\S]*)<\/ul>/Ui', $result, $matches)){
-			$result = trim($matches[1][rand(0, count($matches[1])-1)]);
-			preg_match('/<a href="(.+)"(.+)title="(.+)"/Ui', $result, $match);
-			$uri = $match[1];
-			$title = $match[3];
-			preg_match('/<img data-original="(.+)"/Ui', $result, $match);
-			$imgSrc = $match[1];
-			preg_match('/<p>([\s\S]*)<\/p>/Ui', $result, $match);
-			$desc = $match[1].'...';
-			// 回复图文消息
-			$input['send']['MsgType'] = 'news';
-			$input['send']['ArticleCount'] = 1;
-			$input['send']['Articles'] = array(
-				'item' => array(
-					'Title'=>$title,
-					'Description'=>$desc,
-					'PicUrl'=>$imgSrc,
-					'Url'=>site_url('lookmw.php?uri=').$uri, //本地详情页面
-					),
-				);
-			return $input;
-		}
-	}
-});
-
 /** 新闻 */
 add_action('WeixinDev.message', function($input){
 	$recvContent = &$input['recv']['Content'];
 	$sendContent = &$input['send']['Content'];
 	if(($input['send']['MsgType'] == 'text' && $sendContent) || !empty($input['send']['Articles'])) return;
-	if(strpos($recvContent, '新闻') !== false || strpos($recvContent, '资讯') !== false){
-		$result = curl(array(
-			'url'=>'http://news.sina.cn/',
+	if(str_contents($recvContent, '新闻') && str_contents($recvContent, array('最近','近日','今日','今天','查看'))){
+		// 新浪新闻接口
+		$url = 'https://cre.dp.sina.cn/api/v3/get?cateid=1o&cre=tianyi&mod=wnews&merge=3&statics=1&length=20&tm=1489716199&ad={%22rotate_count%22:21,%22page_url%22:%22https%3A%2F%2Fnews.sina.cn%2F%22,%22channel%22:%22131250%22,%22platform%22:%22wap%22,%22timestamp%22:1496916969233}&action=0&up=0&down=0&length=18&_=1496916969251';
+		$arg = array(
+			'url'=>$url,
 			'userAgent'=>'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
 			'followLocation'=>1,
-			));
-		if(preg_match('/<section class="card_module" id="top_feed" data-sudaclick="feedlist_conf_4">([\s\S]*)<\/section>/Ui', $result, $match)){
-			if(preg_match_all('/<a href="(.*)"(.*)>([\s\S]*)<\/a>/Ui', $match[1], $matches)){ //获取新闻列表
-				$urls = array_map(function($v){ //新闻链接
-					return substr($v, 0, strpos($v, '?'));
-				}, $matches[1]);
-				$imgs = $titles = array();
-				foreach ($matches[3] as $html) { //获取列表数据
-					if(preg_match('/<img(.*)data-src="(.*)"(.*)alt="(.*)">/Ui', $html, $match)){
-						$imgs[] = $match[2]; //缩略图
-						$titles[] = $match[4]; //标题
-					}
-				}
-				$start = rand(0, count($urls)-5); //随机开始抽取新闻
-				$input['send']['MsgType'] = 'news';
-				$input['send']['ArticleCount'] = 4; //4条消息
-				$input['send']['Articles'] = array();
-				$input['send']['Articles']['item'] = array();
-				for ($i=$start; $i < $start+4; $i++) { 
-					$input['send']['Articles']['item'][] = array( //图文消息列表
-						'Title'=>$titles[$i],
-						'Description'=>'',
-						'PicUrl'=>$imgs[$i],
-						'Url'=>$urls[$i],
-					);
-				}
-				return $input;
+			'parseJSON'=>true,
+			);
+		$result  = curl($arg);
+		if(!curl_info('error')){
+			$data = $result['data'];
+			$start = rand(0, count($data)-5);
+			$input['send']['MsgType'] = 'news';
+			$input['send']['ArticleCount'] = 4; //4条消息
+			$input['send']['Articles'] = array();
+			$input['send']['Articles']['item'] = array();
+			for ($i=$start; $i < $start+4; $i++) { 
+				$PicUrl = @$data[$i]['mthumbs'][0] ?: $data[$i]['thumb'];
+				$input['send']['Articles']['item'][] = array( //图文消息列表
+					'Title'=>$data[$i]['title'],
+					'Description'=>$data[$i]['mintro'],
+					'PicUrl'=>$PicUrl,
+					'Url'=>$data[$i]['url'],
+				);
 			}
+			return $input;
 		}
 	}
 });
@@ -281,17 +235,17 @@ add_action('WeixinDev.message', function($input){
 	$recvContent = &$input['recv']['Content'];
 	$sendContent = &$input['send']['Content'];
 	if(($input['send']['MsgType'] == 'text' && $sendContent) || !empty($input['send']['Articles'])) return;
-	if(strpos($recvContent, '电影') !== false || strpos($recvContent, '影片') !== false){
+	if(str_contents($recvContent, '电影') && str_contents($recvContent, array('最近','近日','今日','今天'))){
 		$baseUrl = 'https://mdianying.baidu.com';
 		$result = curl(array( //CURL 请求
-			'url'=>$baseUrl.'/?sfrom=newnuomi&source=nuomi&c=142&sub_channel=nuomi_wap_rukou4',
+			'url'=>$baseUrl,
 			'userAgent'=>'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
 			'followLocation'=>1,
 			));
-		if(preg_match_all('/<a class="item" href="\/movie\/detail\?movieId=([\s\S]*)<\/a>/Ui', $result, $matches)){ //获取电影列表
+		if(!curl_info('error') && preg_match_all('/href="\/movie\/detail\?movieId=([\s\S]*)<\/a>/Ui', $result, $matches)){ //获取电影列表
 			$urls = $imgs = $titles = array();
 			foreach ($matches[0] as $html) { //获取列表数据
-				if(preg_match('/<a class="item" href="(.*)"/Ui', $html, $match) && preg_match('/url\((.*)\);/Ui', $html, $match2)){
+				if(preg_match('/href="(.*)"/Ui', $html, $match) && preg_match('/url\((.*)\);/Ui', $html, $match2)){
 					$urls[] = $baseUrl.$match[1]; //电影链接
 					$url = substr($match2[1], strpos($match2[1], '?')+1);
 					parse_str($url, $get);
@@ -301,12 +255,12 @@ add_action('WeixinDev.message', function($input){
 				}
 			}
 			if(!empty($urls)){
-				$count = count($urls);
+				$start = rand(0, count($urls)-5);
 				$input['send']['MsgType'] = 'news';
-				$input['send']['ArticleCount'] = $count;
+				$input['send']['ArticleCount'] = 4;
 				$input['send']['Articles'] = array();
 				$input['send']['Articles']['item'] = array();
-				for ($i=0; $i < $count; $i++) { 
+				for ($i=$start; $i < $start+4; $i++) { 
 					$input['send']['Articles']['item'][] = array( //多则图文消息
 						'Title'=>$titles[$i],
 						'Description'=>'',
@@ -360,7 +314,75 @@ add_action('WeixinDev.message', function($input){
 	}
 });
 
-// 使用百度获取答案
+// 日期时间处理
+add_action('WeixinDev.message', function($input){
+	$recvContent = &$input['recv']['Content'];
+	$sendContent = &$input['send']['Content'];
+	if(($input['send']['MsgType'] == 'text' && $sendContent) || !empty($input['send']['Articles'])) return;
+	if(str_contents($recvContent, array('今天', '今日')) && str_contents($recvContent, array('星期几', '周几', '什么日子', '几号'))){
+		$week = array('日','一','二','三','四','五','六');
+		$date = date('Y 年 m 月 d 日');
+		$sendContent = '今天是 '.$date.'，星期'.$week[date('w')].'。';
+		return $input;
+	}elseif(str_contents($recvContent, '现在') && str_contents($recvContent, array('几点', '什么时候'))){
+		$sendContent = '现在是北京时间 '.date('H 点 i 分').'。';
+		return $input;
+	}
+});
+
+// 天气/位置服务
+add_action('WeixinDev.message', function($input){
+	global $WxConf;
+	$recvContent = &$input['recv']['Content'];
+	$sendContent = &$input['send']['Content'];
+	if(($input['send']['MsgType'] == 'text' && $sendContent) || !empty($input['send']['Articles'])) return;
+	if(preg_match('/(.*)天气(怎么样|如何)/', $recvContent, $match)){
+		$city = $match[1];
+		if(($i = strpos($city, '今天')) === 0 || ($i = strpos($city, '今日')) === 0)
+			$city = substr($city, $i+6);
+		if(($j = strrpos($city, '今天的')) !== false || ($j = strrpos($city, '今日的')) !== false || ($j = strrpos($city, '今天')) !== false || ($j = strrpos($city, '今日')) !== false)
+			$city = substr($city, 0, $j);
+		if(!$city) $city = $match[1];
+		$cityCode = include(__dir__.'/city-code.php');
+		$weather = array();
+		if(isset($cityCode[$city])){
+			$arg = array(
+				'url'=>'http://www.weather.com.cn/data/cityinfo/'.$cityCode[$city].'.html',
+				'userAgent'=>'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+				'parseJSON'=>true,
+				);
+			$temp = curl($arg);
+			if(!curl_info('error')){
+				$weather = array_merge($weather, $temp['weatherinfo']);
+			}
+			$arg['url'] = 'http://www.weather.com.cn/data/sk/'.$cityCode[$city].'.html';
+			$wind = curl($arg);
+			if(!curl_info('error')){
+				$weather = array_merge($weather, $wind['weatherinfo']);
+			}
+			if($weather){
+				$sendContent = $weather['city'].'今日'.$weather['weather'].'，'.$weather['temp1'].' ~ '.$weather['temp2'].'，'.$weather['WD'].$weather['WS'].'。';
+				return $input;
+			}
+		}
+	}elseif(str_contents($recvContent, array('我在哪','我的位置')) && $WxConf['baiduMapKey']){
+		// 图文消息
+		$imgSrc = $match[1];
+		$input['send']['MsgType'] = 'news';
+		$input['send']['ArticleCount'] = 1;
+		$input['send']['Articles'] = array(
+			'item' => array(
+				'Title'=>'在地图上查看我的位置',
+				'Description'=>'本服务由百度地图提供。',
+				'PicUrl'=>template_url('map.png'),
+				'Url'=>site_url('map.php'),
+				),
+			);
+		return $input;
+	}
+});
+
+// 使用百度/360搜索获取答案
 add_action('WeixinDev.message', function($input){
 	$recvContent = &$input['recv']['Content'];
 	$sendContent = &$input['send']['Content'];
@@ -370,12 +392,12 @@ add_action('WeixinDev.message', function($input){
 	if(!str_contents($recvContent, $keys) || !rand(0, 3)){ // 1/4 概率忽略本地答案而从百度获取
 		// 获取关键字
 		if(!rand(0, 3) && preg_match('/什么叫做(.*)/', $recvContent, $match)){
-			$wd = rtrim($match[1], '吗么麽吧啊？');
-		}elseif(preg_match('/(什么叫|什么是|啥叫|啥是|谁是|哪个是)(.*)/', $recvContent, $match)){
-			$wd = rtrim($match[2], '吗么麽吧啊？');
+			$wd = rtrim($match[1], '吗么麽吧啊？。');
+		}elseif(preg_match('/(什么叫|什么是|啥叫|啥是|谁是|哪个是|介绍一下)(.*)/', $recvContent, $match)){
+			$wd = rtrim($match[2], '吗么麽吧啊？。');
 		}elseif(preg_match('/(知道|了解|认识|听说过|晓得|懂|会|看过|见过)(.*)(吗|么|麽|吧|啊)/U', $recvContent, $match)){
 			$wd = $match[2];
-		}elseif(preg_match('/(.*)是(什么|谁|哪|啥|何|？)/U', $recvContent, $match)){
+		}elseif(preg_match('/(.*)是(什么|谁|哪|啥|何|怎么回事|怎么一回事|？)/U', $recvContent, $match)){
 			$wd = $match[1];
 		}else{
 			$wd = $recvContent;
@@ -400,52 +422,86 @@ add_action('WeixinDev.message', function($input){
 					$sendContent = null;
 			}
 		}
-		// 百度知道
-		if(!$sendContent || !rand(0, 3)){ // 1/4 概率摒弃百度百科答案而从百度知道获取
-			$wd = trim($wd);
-			$arg['url'] = 'http://zhidao.baidu.com/search?word='.urlencode($wd);
-			$data = curl($arg);
-			if(!curl_info('error') && preg_match_all('/<dl.*>([\s\S]*)<\/dl>/U', $data, $matches)){ //获取答案列表
-				array_walk($matches[1], function(&$v){
-					$v = trim(strip_tags($v)); //获取纯文本
-				});
-				$data = array();
-				foreach ($matches[1] as $k => $match) {
-					$match = str_replace("\r\n", "\n", $match);
-					if(($i = strpos($match, "推荐答案")) !== false){ //有推荐答案
-						$start = $i+strlen("推荐答案");
-						$length = strrpos($match, '。')-$start;
-						if($length <= 0)
-							$length = strrpos($match, '...')-$start;
-						if($length <= 0)
-							$length = strpos($match, "[详细]")-$start-3;
-						if($length > 0){
-							$sendContent = trim(substr($match, $start, $length+3));
+		if(!$sendContent || !rand(0,3)){ // 1/4 概率摒弃百度百科答案而从百度知道获取
+			$level = rand(0,2);
+			$strcmp = rand(0, 3);
+			$data = array();
+			if($level){ //百度知道
+				$wd = trim($wd);
+				$arg['url'] = 'http://zhidao.baidu.com/search?word='.urlencode($wd);
+				$result = curl($arg);
+				if(!curl_info('error') && preg_match_all('/<dl.*>([\s\S]*)<\/dl>/U', $result, $matches)){ //获取答案列表
+					array_walk($matches[1], function(&$v){
+						$v = trim(strip_tags($v)); //获取纯文本
+					});
+					foreach ($matches[1] as $k => $match) {
+						$match = str_replace("\r\n", "\n", $match);
+						if(($i = strpos($match, "推荐答案")) !== false){ //有推荐答案
+							$start = $i+strlen("推荐答案");
+							$length = strrpos($match, '。')-$start;
+							if($length <= 0)
+								$length = strrpos($match, '...')-$start;
+							if($length <= 0)
+								$length = strpos($match, "[详细]")-$start-3;
+							if($length > 0){
+								$sendContent = trim(substr($match, $start, $length+3));
+							}else{
+								$sendContent = trim(substr($match, $start));
+							}
+							break;
 						}else{
-							$sendContent = trim(substr($match, $start));
-						}
-						break;
-					}else{
-						$match = ltrim(substr($match, strpos($match, '">')+2)) ?: $match;
-						$title = rtrim(substr($match, 0, strpos($match, "\n")+1));
-						$i = strpos($match, '答：');
-						if($i !== false){ //获取网友回答
-							$match = substr($match, $i+strlen("答："));
-							$match = trim(substr($match, 0, strpos($match, "\n\n"))); //获取回答
-							if(strpos($match, '你好， 3.0版本') !== 0 && strpos($match, 'Du知道君是') !== 0){ //只使用有效回答
-								if(strlen($match) > 3){ //只选择大于 1 个中文的答案
-									// $index = abs(strcasecmp($title, $wd));
-									// $data[$index] = $match;
-									$data[] = $match;
+							$match = ltrim(substr($match, strpos($match, '">')+2)) ?: $match;
+							$title = rtrim(substr($match, 0, strpos($match, "\n")+1));
+							$i = strpos($match, '答：');
+							if($i !== false){ //获取网友回答
+								$match = substr($match, $i+strlen("答："));
+								$match = trim(substr($match, 0, strpos($match, "\n\n"))); //获取回答
+								if(strpos($match, '你好， 3.0版本') !== 0 && strpos($match, 'Du知道君是') !== 0){ //只使用有效回答
+									if(strlen($match) > 3){ //只选择大于 1 个中文的答案
+										if(!$strcmp){
+											$index = abs(strcasecmp($title, $wd));
+											$data[$index] = $match;
+										}else{
+											$data[] = $match;
+										}
+									}
 								}
 							}
 						}
 					}
+					if(!$sendContent && $data){
+						if(!$strcmp){
+							ksort($data);
+							$sendContent = array_shift($data);
+						}else{
+							$sendContent = $data[rand(0, count($data)-1)];
+						}
+					}
 				}
-				if(!$sendContent && $data){
-					// ksort($data);
-					// echo $sendContent = array_shift($data);
-					$sendContent = $data[rand(0, count($data)-1)];
+			}else{ //360问答
+				$arg['url'] = 'http://wenda.so.com/search/?q='.urlencode($wd);
+				$result = curl($arg);
+				if(preg_match_all('/<li class="item">([\s\S]*)<\/li>/U', $result, $matches)){
+					foreach($matches[1] as $v){
+						$v = explode('<div class="qa-i-bd">', $v);
+						$v[0] = strip_tags($v[0]);
+						$v[1] = substr($v[1], 0, strpos($v[1], '<div class="qa-i-ft">'));
+						$v[1] = strip_tags($v[1]);
+						$Q = $v[0];
+						$A = $v[1];
+						if($strcmp){
+							$index = abs(strcasecmp($Q, $wd));
+							$data[$index] = $A;
+						}else{
+							$data[] = $A;
+						}
+					};
+					if(!$strcmp){
+						ksort($data);
+						$sendContent = array_shift($data);
+					}else{
+						$sendContent = $data[rand(0, count($data)-1)];
+					}
 				}
 			}
 		}
--- user/config/weixin-config.php
+++ user/config/weixin-config.php
@@ -6,5 +6,6 @@ return array(
 	'url'=>'', //服务器配置中的 url
 	'token'=>'', //服务器配置中的 Token
 	'appId'=>'',
-	'appSecret'=>''
+	'appSecret'=>'',
+	'baiduMapKey'=>'', //百度地图服务密钥，没有就留空
 	);
